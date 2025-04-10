8388d7d92f124a17a5bb5809eb55ef25b324ea71
sekiguchi <sekiguchi@framelunch.jp>
2025-04-10 13:08:15 +0900

    footer

--- themes/newfiction/footer.php
+++ themes/newfiction/footer.php
@@ -1,4 +1,5 @@
 <?php get_template_part('template-parts/contact'); ?>
+<?php get_template_part('template-parts/global', 'footer'); ?>
 <?php wp_footer(); ?>
 </body>
 </html>
---
+++ themes/newfiction/template-parts/global-footer.php
@@ -0,0 +1,41 @@
+<footer>
+    <div class="capabilities px-4 py-24 flex justify-between w-full">
+        <h2 class="w-1/3 text-5xl font-medium">Capabilities</h2>
+        <div class="w-2/3">
+            <h3>PRODUCT</h3>
+            <div class="flex mt-4">
+                <ul class="w-1/2">
+                    <li>Product Strategy</li>
+                    <li class="mt-2">User Research</li>
+                </ul>
+                <ul class="w-1/2">
+                    <li>Product Strategy</li>
+                    <li class="mt-2">User Research</li>
+                </ul>
+            </div>
+            <h3 class="mt-12">BRAND</h3>
+            <div class="flex mt-4">
+                <ul class="w-1/2">
+                    <li>Product Strategy</li>
+                    <li class="mt-2">User Research</li>
+                </ul>
+                <ul class="w-1/2">
+                    <li>Product Strategy</li>
+                    <li class="mt-2">User Research</li>
+                </ul>
+            </div>
+            <a href="" class="our-services mt-12 flex justify-center items-center w-[140px] h-[52px] font-medium rounded-lg">Our services</a>
+        </div>
+    </div>
+    <div class="foot px-4 pt-12 pb-4">
+        <h2 class="text-[80px]">A new project, an idea, or an intro. <br />Shoot us a message.</h2>
+        <img class="logo" src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="">
+        <div class="flex justify-between mt-4">
+            <div class="links flex">
+                <a class="text-[17px] underline" href="" target="_blank">business@newfiction.org</a>
+                <a class="text-[17px] underline ml-6" href="" target="_blank">LinkedIn</a>
+            </div>
+            <small class="text-[17px]">&copy; 2025 New Fiction</small>
+        </div>
+    </div>
+</footer>
