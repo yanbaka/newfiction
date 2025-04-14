253e5c8af064c13afd85a4b3dfc39bb943409d89
sekiguchi <sekiguchi@framelunch.jp>
2025-04-14 15:34:34 +0900

    update

--- themes/newfiction/index.php
+++ themes/newfiction/index.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
 <main role="main" id="main" class="main" data-barba="container" data-barba-namespace="home">
-	<p class="px-4 pt-[200px] pb-16 text-[80px] fontSelectaMedium">Product & Brand Design Studio.<br />Based in Tokyo, Working Globally.<p>
-	<div class="overview mt-[100vh] px-4 py-[250px]">
+	<p class="px-4 pt-[200px] pb-8 text-[80px] bg-white fontSelectaMedium">Product & Brand Design Studio.<br />Based in Tokyo, Working Globally.<p>
+	<div class="h-screen"></div>
+	<div class="h-screen"></div>
+	<div class="overview px-4 py-[250px] min-h-screen">
 		<p class="lead">日本のデジタルプロダクトとブランドを世界に響く形へ。文化的な感性を大切にしながら、その価値を引き上げます</p>
 		<p class="text mt-4">日本ブランドを世界基準へと磨き上げるときも、海外ブランドの日本市場進出を支援するときも、それぞれの文脈に合わせた最適なアプローチを提供します。</p>
 		<a class="see-the-work rounded-full mt-8">See the work</a>
@@ -11,6 +13,7 @@
 	<div class="overview px-4 py-40">
 		<p class="lead">多様な業界経験を活かし、あらゆる規模の企業と協業。プロダクトとブランドの掛け算で価値を生み出します</p>
 	</div>
+	<div class="h-screen"></div>
 </main><!-- /#main -->
 <div class="background-image top-0 absolute z-[1]">
 	<div class="sticky top-[87px] h-[calc(100vh-87px)]">
