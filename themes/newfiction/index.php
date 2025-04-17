bc6e137486dd35797ce0b1ef18c936d1feff436c
sekiguchi <sekiguchi@framelunch.jp>
2025-04-17 15:29:22 +0900

    spサイズ

--- themes/newfiction/index.php
+++ themes/newfiction/index.php
@@ -7,21 +7,23 @@
 	data-id="top"
 >
 	<div class="relative z-[2]">
-		<p class="px-4 pt-[200px] pb-8 text-[80px] bg-white flex flex-col fontSelectaMedium">
+		<p class="px-4 py-6 text-4xl md:pt-[200px] md:pb-8 md:text-[80px] bg-white flex flex-col fontSelectaMedium">
 			<span class="inline-block -enterAnimation"><span data-animation-type="y">Product & Brand Design Studio.</span></span>
 			<span class="inline-block -enterAnimation"><span data-animation-type="y" class="delay-100">Based in Tokyo, Working Globally.</span></span>
 		<p>
 		<div class="h-screen"></div>
 		<div class="h-screen bg-trigger" data-bg-trigger-id="1" data-bg-type="clip"></div>
-		<div class="overview px-4 py-[250px] min-h-screen bg-trigger" data-bg-trigger-id="2" data-bg-type="changer">
-			<p class="lead">日本のデジタルプロダクトとブランドを世界に響く形へ。文化的な感性を大切にしながら、その価値を引き上げます</p>
-			<p class="text mt-4">日本ブランドを世界基準へと磨き上げるときも、海外ブランドの日本市場進出を支援するときも、それぞれの文脈に合わせた最適なアプローチを提供します。</p>
+		<div class="overview px-4 py-16 md:py-[250px] min-h-screen bg-trigger" data-bg-trigger-id="2" data-bg-type="changer">
+			<div>
+				<p class="lead">日本のデジタルプロダクトとブランドを世界に響く形へ。文化的な感性を大切にしながら、その価値を引き上げます</p>
+				<p class="text mt-4">日本ブランドを世界基準へと磨き上げるときも、海外ブランドの日本市場進出を支援するときも、それぞれの文脈に合わせた最適なアプローチを提供します。</p>
+			</div>
 			<a class="see-the-work rounded-full mt-8">See the work</a>
 		</div>
 		<div class="h-screen"></div>
 		<div class="bg-trigger" data-bg-trigger-id="3" data-bg-type="changer">
 			<?php include get_template_directory() . '/components/flow.php'; ?>
-			<div class="overview px-4 py-40">
+			<div class="overview overview2 px-4 md:py-40 min-h-screen md:min-h-auto">
 				<p class="lead">多様な業界経験を活かし、あらゆる規模の企業と協業。プロダクトとブランドの掛け算で価値を生み出します</p>
 			</div>
 		</div>
--- themes/newfiction/template-parts/global-footer.php
+++ themes/newfiction/template-parts/global-footer.php
@@ -1,41 +1,41 @@
 <footer>
-    <div class="capabilities px-4 py-24 flex justify-between w-full relative z-[2]">
-        <h2 class="w-1/3 text-5xl fontSelectaMedium">Capabilities</h2>
-        <div class="w-2/3">
+    <div class="capabilities px-4 py-12 md:py-24 md:flex md:justify-between w-full relative z-[2]">
+        <h2 class="md:w-1/3 text-5xl fontSelectaMedium">Capabilities</h2>
+        <div class="mt-12 md:mt-0 md:w-2/3">
             <h3>PRODUCT</h3>
-            <div class="flex mt-4">
-                <ul class="w-1/2">
+            <div class="md:flex mt-4">
+                <ul class="md:w-1/2">
                     <li>Product Strategy</li>
                     <li class="mt-2">User Research</li>
                 </ul>
-                <ul class="w-1/2">
-                    <li>Product Strategy</li>
+                <ul class="md:w-1/2">
+                    <li class="mt-2 md:mt-0">Product Strategy</li>
                     <li class="mt-2">User Research</li>
                 </ul>
             </div>
             <h3 class="mt-12">BRAND</h3>
-            <div class="flex mt-4">
-                <ul class="w-1/2">
-                    <li>Product Strategy</li>
+            <div class="md:flex mt-4">
+                <ul class="md:w-1/2">
+                    <li class="mt-2 md:mt-0">Product Strategy</li>
                     <li class="mt-2">User Research</li>
                 </ul>
-                <ul class="w-1/2">
+                <ul class="md:w-1/2">
                     <li>Product Strategy</li>
                     <li class="mt-2">User Research</li>
                 </ul>
             </div>
-            <a href="<?php echo esc_url(home_url('/work')); ?>" class="our-services mt-12 flex justify-center items-center w-[140px] h-[52px] rounded-full">See the work</a>
+            <a href="<?php echo esc_url(home_url('/about')); ?>" class="our-services mt-12 flex justify-center items-center w-full h-[44px] md:w-[140px] md:h-[52px] rounded-full">More about us</a>
         </div>
     </div>
-    <div class="foot px-4 pt-12 pb-4 fixed bottom-0">
-        <h2 class="text-[80px]">A new project, an idea, or an intro. <br />Shoot us a message.</h2>
+    <div class="foot px-4 pt-8 md:pt-12 pb-4 fixed bottom-0">
+        <h2 class="text-[40px] md:text-[80px]">A new project, an idea, or an intro. <br />Shoot us a message.</h2>
         <img class="logo" src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="">
-        <div class="flex justify-between mt-4">
-            <div class="links flex">
+        <div class="md:flex md:justify-between mt-[12px] md:mt-4">
+            <div class="links flex justify-between md:justify-start">
                 <a class="text-[17px] underline" href="" target="_blank">business@newfiction.org</a>
                 <a class="text-[17px] underline ml-6" href="" target="_blank">LinkedIn</a>
             </div>
-            <small class="text-[17px]">&copy; 2025 New Fiction</small>
+            <small class="block mt-[12px] md:mt-0 text-[17px]">&copy; 2025 New Fiction</small>
         </div>
     </div>
 </footer>
